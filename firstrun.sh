e3b6f5c7507cf9b37675d440ef085ef77f85a6d1
Olavi Haapala <ojhaapala@gmail.com>
2016-07-16 15:00:14 +0300

    Clone into current dir instead of home

--- firstrun.sh
+++ firstrun.sh
@@ -10,6 +10,21 @@ if [[ "$answer" != "y" ]]; then
 fi
 
 # janus vim
-curl -L "https://bit.ly/janus-bootstrap" | bash
+function die()
+{
+    echo "${@}"
+    exit 1
+}
+
+# Add .old to any existing Vim file in the directory
+for filepath in ".vim" ".vimrc" ".gvimrc"; do
+  if [ -e $filepath ]; then
+    mv "${filepath}" "${filepath}.old" || die "Could not move ${filepath} to ${filepath}.old"
+    echo "${filepath} has been renamed to ${filepath}.old"
+  fi
+done
+
+# Clone Janus into .vim
+git clone --recursive https://github.com/carlhuda/janus.git ".vim"
 
 ./update.sh
