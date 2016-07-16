a3cf1a5d5dacd56da841167af34b39968088dd3a
Olavi Haapala <ojhaapala@gmail.com>
2016-07-16 14:28:20 +0300

    Fix typo

--- firstrun.sh
+++ firstrun.sh
@@ -10,6 +10,6 @@ if [[ "$answer" != "y" ]]; then
     fi
 
     # janus vim
-    curl -L https://bit.ly/janus-bootstrap | bash//bit.ly/janus-bootstrap | bashddd "$(dirname $0)"
+    curl -L https://bit.ly/janus-bootstrap | bash//bit.ly/janus-bootstrap | bash "$(dirname $0)"
 
     ./update.sh
