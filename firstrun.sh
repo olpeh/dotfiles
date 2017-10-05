23895125bd5e2140ac80640718b5400924094d54
Olavi Haapala <ojhaapala@gmail.com>
2017-10-05 10:40:47 +0300

    Add fzf

--- firstrun.sh
+++ firstrun.sh
@@ -31,4 +31,9 @@ git clone https://github.com/tmux-plugins/tmux-resurrect ".tmux-resurrect"
 # Install oh-my-szh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install -y
+
 ./update.sh
