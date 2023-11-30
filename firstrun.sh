9e8e53e54e2a31467c565f8e51ad179067a0a19d
Olavi Haapala <mail@olavihaapala.fi>
2023-11-30 14:12:37 +0200

    Add back fzf

--- firstrun.sh
+++ firstrun.sh
@@ -27,4 +27,9 @@ done
 # Install oh-my-szh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install -y
+
 ./update.sh
